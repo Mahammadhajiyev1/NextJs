88d843a1a67bfdd9f72946877df96c131895f7f9
Mahammadhajiyev1 <Mahammadhajiyev1@gmail.com>
2021-09-09 22:23:29 +0400

    initial commit

---
+++ src/constants/constants.js
@@ -0,0 +1,49 @@
+export const projects = [
+  {
+    title: "Company Website",
+    description:
+      "Using React, Next.js we build this amazing website for our favorite costumer. It is singla page website very light and responsive.",
+    image: "/images/1.png",
+    tags: ["React", "Next"],
+    source: "https://mahammadhajiyev.com/enderdirican/",
+    visit: "https://google.com",
+    id: 0,
+  },
+  {
+    title: "The Pig Game",
+    description:
+      "This game made with just javascript. It is small and enjoyabe game",
+    image: "/images/2.png",
+    tags: ["JavaScript"],
+    source: "https://google.com",
+    visit: "https://google.com",
+    id: 1,
+  },
+  {
+    title: "Guess Number",
+    description: "Retro game feeling and fun to play",
+    image: "/images/3.jpg",
+    tags: ["JavaScript"],
+    source: "https://google.com",
+    visit: "https://google.com",
+    id: 2,
+  },
+  {
+    title: "Unichat",
+    description:
+      "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
+    image: "/images/4.jpg",
+    tags: ["React", "ChatEngine", "Firebase"],
+    source: "https://google.com",
+    visit: "https://google.com",
+    id: 3,
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
