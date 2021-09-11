870ccb31a090a70f6b4c9d208fff05da579e7073
Mahammad Hajiyev <MahammadHajiyev1@gmail.com>
2021-09-12 02:54:02 +0400

    initial Commit

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React from "react";
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
 
 const Hero = (props) => (
   <>
@@ -13,7 +17,8 @@ const Hero = (props) => (
           My Personal Portfolio
         </SectionTitle>
         <SectionText>
-        The purpose of JavaScript Mastery is to help aspiring and established developers to take their development skills to the next level and build awesome apps.
+          I am professional Full Stack developer. I cooperate with multiple
+          companies and individuals for their projects.
         </SectionText>
         <Button onClick={props.handleClick}>Learn More</Button>
       </LeftSection>
