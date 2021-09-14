73097d9f815bcc06e6917a8fc6118e72a8dafb1a
Mahammad Hajiyev <MahammadHajiyev1@gmail.com>
2021-09-14 13:36:30 +0400

    Last changes

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -28,22 +28,12 @@ export const projects = [
     visit: "https://google.com",
     id: 2,
   },
-  {
-    title: "Unichat",
-    description:
-      "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
-    image: "/images/4.jpg",
-    tags: ["React", "ChatEngine", "Firebase"],
-    source: "https://google.com",
-    visit: "https://google.com",
-    id: 3,
-  },
 ];
 
 export const TimeLineData = [
-  { year: 2017, text: "Started my journey" },
-  { year: 2018, text: "Worked as a freelance developer" },
-  { year: 2019, text: "Founded JavaScript Mastery" },
-  { year: 2020, text: "Shared my projects with the world" },
-  { year: 2021, text: "Started my own platform" },
+  { year: 2013, text: "Started my first job in furniture factory" },
+  { year: 2016, text: "Started and learned 3D design" },
+  { year: 2018, text: "Worked as freelancer for companies" },
+  { year: 2019, text: "Started Web development" },
+  { year: 2020, text: "Now I am full stack developer" },
 ];
